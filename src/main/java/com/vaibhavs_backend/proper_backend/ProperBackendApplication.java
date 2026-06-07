9a7c0e410bc8627da60d92c40da0d8f8293135e9
S Vaibhav <s.vaibhav15656@gmail.com>
2026-06-08 00:43:47 +0530

    initial project setup

---
+++ src/main/java/com/vaibhavs_backend/proper_backend/ProperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhavs_backend.proper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProperBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhavs_backend/proper_backend/ProperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavs_backend.proper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
