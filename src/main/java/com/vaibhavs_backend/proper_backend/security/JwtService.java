7e8cf8d51db02fbfccbcb39a2f6f89cc7b979926
S Vaibhav <s.vaibhav15656@gmail.com>
2026-06-12 02:42:36 +0530

    completed JwtService

--- src/main/java/com/vaibhavs_backend/proper_backend/security/JwtService.java
+++ src/main/java/com/vaibhavs_backend/proper_backend/security/JwtService.java
@@ -1,25 +1,56 @@
 package com.vaibhavs_backend.proper_backend.security;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.vaibhavs_backend.proper_backend.entity.User;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 @Service
 public class JwtService {
-    @Value("${jwt.secret}")    
+    @Value("${jwt.secret}")
     private String secretKey;
-    public String generateToken(User user) {
-    // leave empty for now
-    return null;
-}
-
-public String extractEmail(String token) {
-    return null;
-}
-
-public boolean isTokenValid(String token, UserDetails userDetails) {
-    return false;
-}
-
+    public String generateToken(User user){
+        return Jwts.builder()
+        .subject(user.getEmail())
+        .issuedAt(new Date())
+        .expiration(new Date(System.currentTimeMillis()+86400000))
+        .signWith(getSignKey())
+        .compact();
+    }
+    public Boolean isTokenValid(UserDetails userdetails , String token){
+        String email = extractEmail(token);
+        return email.equals(userdetails.getUsername()) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token){
+        return Jwts.parser()
+        .verifyWith(getSignKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .getExpiration();
+    }
+    private SecretKey getSignKey(){
+        byte[] KeyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(KeyBytes);
+    }
+    public String extractEmail(String token){
+        return Jwts.parser()
+        .verifyWith(getSignKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload()
+        .getSubject();
+    }
 }
