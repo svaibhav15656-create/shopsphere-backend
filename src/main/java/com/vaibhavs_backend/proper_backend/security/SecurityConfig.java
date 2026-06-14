73e86d3ceea4128b71f6173bf1f4117e1316f210
S Vaibhav <s.vaibhav15656@gmail.com>
2026-06-15 01:31:54 +0530

    completed Security_config successfully

--- src/main/java/com/vaibhavs_backend/proper_backend/security/SecurityConfig.java
+++ src/main/java/com/vaibhavs_backend/proper_backend/security/SecurityConfig.java
@@ -5,35 +5,53 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Autowired
-    private JwtService jwtservice;
+    private JwtAuthFilter jwtauthfilter;
     @Autowired
     private CustomUserDetailsService customuserdeatilsservice;
     
     @Bean
-    public PasswordEncoder passwordencoder(){
-        return null;
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
     @Bean
     public AuthenticationProvider authenticationProvider(){
-        return null;
+        DaoAuthenticationProvider  provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(customuserdeatilsservice);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+
     }
     @Bean
-    public  AuthenticationManager authenticationManager(AuthenticationConfiguration config){
-        return null;
+    public  AuthenticationManager authenticationManager(AuthenticationConfiguration config)throws Exception{
+        return config.getAuthenticationManager();
     }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
-        return null;
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) 
+            )
+            .authenticationProvider(authenticationProvider())
+            .addFilterBefore(jwtauthfilter,UsernamePasswordAuthenticationFilter.class);
+            return http.build();
     }
 }
