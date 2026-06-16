820d1ecbbba395c3974e68e8df1ade09fdd4f8e3
S Vaibhav <s.vaibhav15656@gmail.com>
2026-06-17 00:10:08 +0530

    added api today and completed authController

--- src/main/java/com/vaibhavs_backend/proper_backend/controller/AuthController.java
+++ src/main/java/com/vaibhavs_backend/proper_backend/controller/AuthController.java
@@ -1,16 +1,33 @@
 package com.vaibhavs_backend.proper_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vaibhavs_backend.proper_backend.dto.LoginRequest;
+import com.vaibhavs_backend.proper_backend.dto.RegisterRequest;
 import com.vaibhavs_backend.proper_backend.service.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     private AuthService authseservice;
+    @PostMapping("/register")
+    public ResponseEntity<String> register (@RequestBody RegisterRequest request) {
+        String token = authseservice.register(request);
+        return ResponseEntity.ok("access token is"+token);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+         String token = authseservice.login(request);
+    return ResponseEntity.ok("access token is"+token);
+
+    }
     
     
 }
